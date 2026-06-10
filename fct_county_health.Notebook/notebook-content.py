baef55ccde3fc9189b018a4dcd0b03fcf5faf5c7
alepanti <82419795+alepanti@users.noreply.github.com>
2026-06-10 15:53:23 -0400

    fix gold layer fact table after aggregation done on silver

--- fct_county_health.Notebook/notebook-content.py
+++ fct_county_health.Notebook/notebook-content.py
@@ -30,7 +30,13 @@ except:
         from silver.cdc_places
     """).collect()[0][0])
 
-print(f'Building fact table for CDC {release_year} Release')
+data_year = int(spark.sql(f"""
+    select data_year
+    from dbo.cdc_places_releases
+    where release_year = {release_year}
+""").collect()[0][0])
+
+print(f'Building fact table for CDC {release_year} Release, Based on {data_year} Data')
 
 # METADATA ********************
 
@@ -48,8 +54,6 @@ print(f'Building fact table for CDC {release_year} Release')
 # MAGIC     ,asthma_crude_prev double
 # MAGIC     ,smoke_crude_prev double
 # MAGIC     ,obesity_crude_prev double
-# MAGIC     ,copd_crude_prev double
-# MAGIC     ,insur_access_crude_prev double
 # MAGIC     ,ozone_mean double
 # MAGIC     ,ozone_98p double
 # MAGIC     ,pm25_mean double
@@ -61,6 +65,7 @@ print(f'Building fact table for CDC {release_year} Release')
 # MAGIC     ,pct_hisp_lat double
 # MAGIC     ,pop_density double
 # MAGIC     ,data_year int
+# MAGIC     ,release_year int
 # MAGIC ) using delta;
 
 # METADATA ********************
@@ -72,37 +77,10 @@ print(f'Building fact table for CDC {release_year} Release')
 
 # CELL ********************
 
-spark.sql()
-insert overwrite table gold.fct_county_health
-select distinct
-    cdc.county_fips
-    ,cdc.CASTHMA_CrudePrev as asthma_crude_prev
-    ,cdc.CSMOKING_CrudePrev as smoke_crude_prev
-    ,cdc.OBESITY_CrudePrev as obesity_crude_prev
-    ,cdc.COPD_CrudePrev as copd_crude_prev
-    ,cdc.ACCESS2_CrudePrev as insur_access_crude_prev
-    ,oz.arithmetic_mean as ozone_mean
-    ,oz.ninety_eighth_percentile as ozone_98p
-    ,pm.arithmetic_mean as pm25_mean
-    ,pm.ninety_eighth_percentile as pm25_98p
-    ,cen.med_house_income as median_income
-    ,cen.percent_below_pov as poverty_rate
-    ,cen.percent_white as pct_white
-    ,cen.percent_black_aa as pct_black_aa
-    ,cen.percent_hisp_lat as pct_hisp_lat
-    ,(d.total_pop / d.land_area_sqmi) as pop_density
-    ,cdc.data_year
-from silver.cdc_places cdc 
-left join silver.aqs_oz oz
-    on cdc.county_fips = oz.county_fips
-left join silver.aqs_pm25 pm
-    on cdc.county_fips = pm.county_fips
-left join silver.census cen
-    on cdc.county_fips = cen.county_fips
-left join gold.dim_county d
-    on cdc.county_fips = d.county_fips
-where cdc.release_year = {release_year}
-order by county_fips;
+spark.sql(f"""
+    delete from gold.fct_county_health
+    where data_year = {data_year};
+""")
 
 # METADATA ********************
 
@@ -113,16 +91,87 @@ order by county_fips;
 
 # CELL ********************
 
-# MAGIC %%sql
-# MAGIC select count(distinct county_fips) from gol.fct_county_health;
-# MAGIC select count(distinct *) from sil.cdc_places;
-# MAGIC select count(1) from sil.cdc_places;
-# MAGIC select count(distinct county_fips) from sil.cdc_places;
+spark.sql(f"""
+    insert into gold.fct_county_health
+    select distinct
+        cdc.county_fips
+        ,cdc.CASTHMA_CrudePrev as asthma_crude_prev
+        ,cdc.CSMOKING_CrudePrev as smoke_crude_prev
+        ,cdc.OBESITY_CrudePrev as obesity_crude_prev
+        ,oz.arithmetic_mean as ozone_mean
+        ,oz.ninety_eighth_percentile as ozone_98p
+        ,pm.arithmetic_mean as pm25_mean
+        ,pm.ninety_eighth_percentile as pm25_98p
+        ,cen.med_house_income as median_income
+        ,cen.percent_below_pov as poverty_rate
+        ,cen.percent_white as pct_white
+        ,cen.percent_black_aa as pct_black_aa
+        ,cen.percent_hisp_lat as pct_hisp_lat
+        ,(cdc.total_pop / c.land_area_sqmi) as pop_density
+        ,cdc.data_year
+        ,cdc.release_year
+    from silver.cdc_places cdc 
+    left join silver.aqs_oz oz
+        on cdc.county_fips = oz.county_fips
+        and oz.data_year = {data_year}
+    left join silver.aqs_pm25 pm
+        on cdc.county_fips = pm.county_fips
+        and pm.data_year = {data_year}
+    left join silver.census cen
+        on cdc.county_fips = cen.county_fips
+        and cen.data_year = {data_year}
+    left join silver.gaz_county c
+        on cdc.county_fips = c.county_fips
+    where cdc.release_year = {release_year}
+    order by county_fips;
+""")
+
 
 # METADATA ********************
 
 # META {
-# META   "language": "sparksql",
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+count = spark.sql(f"""
+    select count(*) AS cnt 
+    from gold.fct_county_health 
+    where data_year = {data_year}
+""").collect()[0]['cnt']
+
+null_asthma = spark.sql(f"""
+    select COUNT(*) AS cnt 
+    from gold.fct_county_health 
+    where 
+        data_year = {data_year}
+        and asthma_crude_prev IS NULL
+""").collect()[0]['cnt']
+
+print(f'null asthma count: {null_asthma}')
+print(f'row count for year: {count}')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+spark.sql(f"""
+    select count(1) as silver_count 
+    from silver.cdc_places
+    where data_year = {data_year}
+""").show()
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
 # META   "language_group": "synapse_pyspark"
 # META }
 
@@ -130,22 +179,15 @@ order by county_fips;
 
 # MAGIC %%sql
 # MAGIC 
-# MAGIC select count(1) from gol.fct_county_health
-# MAGIC where asthma_crude_prev is null;
+# MAGIC select count(distinct county_fips) from 
+# MAGIC silver.aqs_oz;
+# MAGIC 
+# MAGIC select count(distinct county_fips) FROM
+# MAGIC silver.aqs_pm25;
 
 # METADATA ********************
 
 # META {
 # META   "language": "sparksql",
-# META   "language_group": "synapse_pyspark"
-# META }
-
-# CELL ********************
-
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
 # META   "language_group": "synapse_pyspark"
 # META }
