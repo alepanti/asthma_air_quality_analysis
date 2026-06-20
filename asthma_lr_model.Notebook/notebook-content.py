f7b8a1979b7a18f0c4188a164ffcc3af74f9b7b7
alepanti <82419795+alepanti@users.noreply.github.com>
2026-06-20 01:31:27 -0400

    linear regression model train and testing

---
+++ asthma_lr_model.Notebook/notebook-content.py
@@ -0,0 +1,271 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "e356aa96-deef-4254-b67d-257cead402cc",
+# META       "default_lakehouse_name": "capstone_lh",
+# META       "default_lakehouse_workspace_id": "f51179d3-a609-4970-b80c-ca7cecaccc24",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "e356aa96-deef-4254-b67d-257cead402cc"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.evaluation import RegressionEvaluator
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+data_df = spark.read.table('capstone_lh.gold.ml_county_health')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+data_df = data_df.drop('county_fips', 'pm25_98p', 'ozone_98p')
+input_col = [
+    'smoke_crude_prev'
+    ,'obesity_crude_prev'
+    ,'ozone_mean'
+    ,'pm25_mean'
+    ,'median_income'
+    ,'poverty_rate'
+    ,'pct_white'
+    ,'pct_black_aa'
+    ,'pct_hisp_lat'
+]
+
+target = 'asthma_crude_prev'
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+assembler = VectorAssembler(inputCols=input_col, outputCol='features')
+
+data_df = assembler.transform(data_df)
+data = data_df.select('features', target)
+
+train_data, test_data = data.randomSplit([0.7, 0.3], seed=42)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+lr = LinearRegression(featuresCol='features', labelCol=target, predictionCol=f'pred_{target}')
+lr_model = lr.fit(train_data)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+predictions = lr_model.transform(test_data)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+eval_rmse = RegressionEvaluator(labelCol=target, predictionCol=f'pred_{target}', metricName='rmse')
+rmse = eval_rmse.evaluate(predictions)
+
+print(f'RMSE on test data: {rmse}')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+eval_r2 = RegressionEvaluator(labelCol=target, predictionCol=f'pred_{target}', metricName='r2')
+r2 = eval_r2.evaluate(predictions)
+
+print(f'R2 on test data: {r2}')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+eval_mae = RegressionEvaluator(labelCol=target, predictionCol=f'pred_{target}', metricName='mae')
+mae = eval_mae.evaluate(predictions)
+
+print(f'MAE on test data: {mae}')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+coeff = lr_model.coefficients
+intercept = lr_model.intercept
+
+print('Coefficients: ', coeff)
+print('Intercetpt: ', intercept)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+ranking = sorted(
+    list(zip(input_col, coeff, map(abs, coeff))),
+    key=lambda x: x[2],
+    reverse=True
+)
+
+print('Linear Regression Coefficient Ranking:')
+for feature, coefficient, rank in ranking:
+    print(f'    {feature}: coefficient = {coefficient}\n      abs_coefficient = {rank}')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+data2_df = spark.read.table('capstone_lh.gold.ml_county_health')
+data2_df = data2_df.drop('county_fips', 'pm25_98p', 'ozone_98p')
+input_col = [
+    'smoke_crude_prev'
+    ,'obesity_crude_prev'
+    ,'ozone_mean'
+    ,'pm25_mean'
+    ,'poverty_rate'
+    ,'pct_white'
+    ,'pct_black_aa'
+    ,'pct_hisp_lat'
+]
+
+target = 'asthma_crude_prev'
+
+assembler = VectorAssembler(inputCols=input_col, outputCol='features')
+
+data2_df = assembler.transform(data2_df)
+data2 = data2_df.select('features', target)
+
+train_data, test_data = data2.randomSplit([0.7, 0.3], seed=42)
+
+lr2 = LinearRegression(featuresCol='features', labelCol=target, predictionCol=f'pred_{target}')
+lr2_model = lr2.fit(train_data)
+
+predictions = lr2_model.transform(test_data)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+eval_rmse = RegressionEvaluator(labelCol=target, predictionCol=f'pred_{target}', metricName='rmse')
+rmse = eval_rmse.evaluate(predictions)
+
+print(f'RMSE on test data: {rmse}')
+
+eval_r2 = RegressionEvaluator(labelCol=target, predictionCol=f'pred_{target}', metricName='r2')
+r2 = eval_r2.evaluate(predictions)
+
+print(f'R2 on test data: {r2}')
+
+eval_mae = RegressionEvaluator(labelCol=target, predictionCol=f'pred_{target}', metricName='mae')
+mae = eval_mae.evaluate(predictions)
+
+print(f'MAE on test data: {mae}')
+
+
+coeff = lr2_model.coefficients
+intercept = lr2_model.intercept
+
+print('Coefficients: ', coeff)
+print('Intercetpt: ', intercept)
+
+ranking2 = sorted(
+    list(zip(input_col, coeff, map(abs, coeff))),
+    key=lambda x: x[2],
+    reverse=True
+)
+
+print('\nLinear Regression Coefficient Ranking with Improved Selection:')
+for feature, coefficient, rank in ranking2:
+    print(f'    {feature}: coefficient = {coefficient}\n      abs_coefficient = {rank}')
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
